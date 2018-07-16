edd6e766af584b575e209fdc7d44e2ef27742888
Jan Schrewe <jan@schafproductions.com>
2018-07-16 22:15:38 +0200

    tt_content ovverride and svg icon

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,8 @@
+<?php
+
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
+    'DUD.' . $_EXTKEY,
+    'Pinnwand',
+    'Pinnwand für kleine Items',
+    'EXT:store_inventory/Resources/Public/Icons/Extension.svg'
+);
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,21 +1,17 @@
 <?php
-if (!defined('TYPO3_MODE')) {
-	die ('Access denied.');
-}
+defined('TYPO3_MODE') || die('Access denied.');
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
-	'DUD.' . $_EXTKEY,
-	'Dudpinnwand',
-	array(
-		'Pinnwand' => 'list',
-		
-	),
-	// non-cacheable actions
-	array(
-		'Pinnwand' => '',
-	)
+    'DUD.' . $_EXTKEY,
+    'Pinnwand',
+    array(
+        'Pinnwand' => 'list',
+
+    ),
+    // non-cacheable actions
+    array(
+        'Pinnwand' => '',
+    )
 );
 
 //$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][$_EXTKEY . '_clearcache'] ='DUD\DudPinnwand\Hooks\Tcemain->clearCachePostProc';
-
-?>
